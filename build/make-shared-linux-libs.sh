8fa28f4687b54b01eadb0866b13ec734cebc3b06
Michael Zeilfelder <info@michaelzeilfelder.de>
2015-03-16 16:30:30 +0100

    Some (non-debian) distros build libs into target lib64 instead of lib on 64-bit systems. Thx @dev-random for reporting. New solution is more of a workaround, but let's see if it works for now. Bugtracker-link: https://bitbucket.org/mzeilfelder/trunk_hc1/issue/1/compilation-isssues

--- build/make-shared-linux-libs.sh
+++ build/make-shared-linux-libs.sh
@@ -12,12 +12,42 @@ SHARED_TARGET=$LIBS_TARGET/linux_shared
 
 rm -rf $SHARED_TARGET
 mkdir $SHARED_TARGET
-cp $LIBS_TARGET/freealut/linux/lib/libalut.so.0 $SHARED_TARGET
-cp $LIBS_TARGET/openal/linux/lib/libopenal.so.1 $SHARED_TARGET
-cp $LIBS_TARGET/sdl/linux/lib/libSDL-1.2.so.0 $SHARED_TARGET
+
+# Better multi-platform support still needs to be done.
+# For now just look in different folders and copy the libs which are found. Complain if they are not found at all.
+if test -d $LIBS_TARGET"/freealut/linux/lib"
+then
+    cp $LIBS_TARGET/freealut/linux/lib/libalut.so.0 $SHARED_TARGET
+elif  test -d "$LIBS_TARGET/freealut/linux/lib64"
+then
+	cp $LIBS_TARGET/freealut/linux/lib64/libalut.so.0 $SHARED_TARGET
+else
+	echo "Missing libalut.so.0"
+fi
+
+if test -d "$LIBS_TARGET/openal/linux/lib"
+then
+	cp $LIBS_TARGET/openal/linux/lib/libopenal.so.1 $SHARED_TARGET
+elif test -d "$LIBS_TARGET/openal/linux/lib64"
+then
+	cp $LIBS_TARGET/openal/linux/lib64/libopenal.so.1 $SHARED_TARGET
+else
+	echo "Missing libopenal.so.1"
+fi
+
+if test -d "$LIBS_TARGET/sdl/linux/lib"
+then
+	cp $LIBS_TARGET/sdl/linux/lib/libSDL-1.2.so.0 $SHARED_TARGET
+elif test -d "$LIBS_TARGET/sdl/linux/lib64"
+then
+	cp $LIBS_TARGET/sdl/linux/lib64/libSDL-1.2.so.0 $SHARED_TARGET
+else
+	echo "Missing libopenal.so.1"
+fi
+
 
 # That was for 32-bit, 64-bit libs are named a little different
-# doesn't matter for now as I don't plan on releasing this version on Linux64 for now, but just has to run locally
+# But really no idea yet how to handle that in a good way on Linux distros which will work in the long run.
 #cp /usr/lib/libstdc++.so.6 $SHARED_TARGET
 #cp /lib/libgcc_s.so.1 $SHARED_TARGET
 #chmod -x $SHARED_TARGET/*
